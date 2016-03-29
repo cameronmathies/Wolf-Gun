c6a47e7ecce8a003f346662b2d743ae3a0ed4500
cameronmathies <cmathies@mr.preble.com>
2016-03-29 13:01:17 -0400

    Gun Cooldown

---
+++ Wolf Gun Project/Assets/Code/Cursor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+	public GUITexture gt;
+	void Start() {
+		gt = GetComponent<GUITexture>();
+	}
+	void DidLockCursor() {
+		Debug.Log("Locking cursor");
+		gt.enabled = false;
+	}
+	void DidUnlockCursor() {
+		Debug.Log("Unlocking cursor");
+		gt.enabled = true;
+	}
+	void OnMouseDown() {
+		Screen.lockCursor = true;
+	}
+	private bool wasLocked = false;
+	void Update() {
+		if (Input.GetKeyDown("escape"))
+			Screen.lockCursor = false;
+		
+		if (!Screen.lockCursor && wasLocked) {
+			wasLocked = false;
+			DidUnlockCursor();
+		} else
+		if (Screen.lockCursor && !wasLocked) {
+			wasLocked = true;
+			DidLockCursor();
+		}
+	}
+}
