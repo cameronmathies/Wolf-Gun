9eaf3aef3f6c97e1b8d6d18e39a8b76570ff001f
cameronmathies <cmathies@mr.preble.com>
2016-03-29 11:49:35 -0400

    Quit
    Before Test

---
+++ Wolf Gun Project/Assets/Code/Quit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour {
+
+	void OnTriggerEnter(){
+		if (GetComponent<Collider>().tag == "Ball"){
+			Application.Quit();
+		}
+}
+}
