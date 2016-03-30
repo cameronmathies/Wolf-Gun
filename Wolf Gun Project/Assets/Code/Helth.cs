224fde058b99ba683fad22da95eda31f5ec00597
cameronmathies <cmathies@mr.preble.com>
2016-03-30 13:12:02 -0400

    AI can kill.

---
+++ Wolf Gun Project/Assets/Code/Helth.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Helth : MonoBehaviour {
+
+	public Slider HealthBar;
+	public int Health;
+	public Text HealthT;
+	public GameObject FireBall;
+	
+	// Use this for initialization
+	void Start () {
+		Health = 100;
+	}
+	void OnTriggerEnter(Collider other)
+	{
+
+			Debug.Log("Die");
+			Health -= 6;
+			//Destroy();
+			
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (HealthBar.name == "Health")
+		{
+			HealthBar.value = Health;
+		}
+		HealthT.text = Health + "";
+		
+		if(Health < 0)
+		{
+			Application.LoadLevel("Main Menuss");
+		}
+	}
+}
