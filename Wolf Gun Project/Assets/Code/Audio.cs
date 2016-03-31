3cfeddd5a8a960ec5cad4bc53f7fca0efba881b3
cameronmathies <cmathies@mr.preble.com>
2016-03-31 13:10:46 -0400

    MedKit
    The medkit sound now has better timming to when health is given.

--- Wolf Gun Project/Assets/Code/Audio.cs
+++ Wolf Gun Project/Assets/Code/Audio.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Audio : MonoBehaviour {
 
 	public AudioSource Hurt;
-
+	public AudioSource MedKit;
+	public int MedKitNumber;
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Bad") {
@@ -15,5 +16,15 @@ public class Audio : MonoBehaviour {
 			Hurt.Play ();
 			
 		}
+		if (other.gameObject.tag == "HP +") {
+			//Debug.Log("Die");
+			StartCoroutine(Med());
+		}
+	}
+	IEnumerator Med(){
+		//Destroy();
+		Hurt = GetComponent<AudioSource> ();
+		yield return new WaitForSeconds(MedKitNumber); 
+		MedKit.Play ();
 	}
 }
