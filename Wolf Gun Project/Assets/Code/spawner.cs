8109294c82ad4a34786d4fa87e13daf566b7f003
cameronmathies <cmathies@mr.preble.com>
2016-04-01 13:26:28 -0400

    4/1/16 Update
    In the proses of making spnwers.

---
+++ Wolf Gun Project/Assets/Code/spawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] whatToSpawnPrefab;
+	public GameObject[] whatToSpawnClone;
+
+	void spawnSomethingAwesomePlease(){
+		whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+}
