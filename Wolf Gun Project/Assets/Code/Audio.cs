b3abc4654ca5f427ad8baaadfb1399aa89c9c59b
cameronmathies <cmathies@mr.preble.com>
2016-04-04 12:38:46 -0400

    Lazer

--- Wolf Gun Project/Assets/Code/Audio.cs
+++ Wolf Gun Project/Assets/Code/Audio.cs
@@ -5,6 +5,7 @@ public class Audio : MonoBehaviour {
 
 	public AudioSource Hurt;
 	public AudioSource MedKit;
+	public AudioSource Lazer;
 	public int MedKitNumber;
 	void OnTriggerEnter(Collider other)
 	{
@@ -20,11 +21,23 @@ public class Audio : MonoBehaviour {
 			//Debug.Log("Die");
 			StartCoroutine(Med());
 		}
+		if (Input.GetButtonDown("Fire1")){
+			//Debug.Log("Die");
+			Hurt = GetComponent<AudioSource> ();
+			Lazer.Play ();
+		}
+		//Destroy();s
+
+		
 	}
+
+
 	IEnumerator Med(){
 		//Destroy();
 		Hurt = GetComponent<AudioSource> ();
 		yield return new WaitForSeconds(MedKitNumber); 
 		MedKit.Play ();
 	}
+
 }
+
