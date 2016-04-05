c875daf6bf0258659a51cb34b0a9d9f4c9f01786
cameronmathies <cmathies@mr.preble.com>
2016-04-05 11:53:42 -0400

    Spawner Works
    It works now just need random and/or timer spawning.

--- Wolf Gun Project/Assets/Code/spawner.cs
+++ Wolf Gun Project/Assets/Code/spawner.cs
@@ -12,5 +12,7 @@ public class spawner : MonoBehaviour {
 	}
 	void spawnSomethingAwesomePlease(){
 		whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		whatToSpawnClone [1] = Instantiate (whatToSpawnPrefab[1], spawnLocations[1].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		whatToSpawnClone [2] = Instantiate (whatToSpawnPrefab[2], spawnLocations[2].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
 	}
 }
