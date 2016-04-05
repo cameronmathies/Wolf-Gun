0e76bb5190dda43b8977c77e01453a335a9b7d39
cameronmathies <cmathies@mr.preble.com>
2016-04-05 12:22:28 -0400

    Timer spawns stuff
    then it kill framrate

--- Wolf Gun Project/Assets/Code/spawner.cs
+++ Wolf Gun Project/Assets/Code/spawner.cs
@@ -6,13 +6,22 @@ public class spawner : MonoBehaviour {
 	public Transform[] spawnLocations;
 	public GameObject[] whatToSpawnPrefab;
 	public GameObject[] whatToSpawnClone;
+	public float myCoolTimer;
+	public float spawn = 0f;
 
 	void Start(){
-		spawnSomethingAwesomePlease ();
+		//spawnSomethingAwesomePlease ();
 	}
 	void spawnSomethingAwesomePlease(){
 		whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
 		whatToSpawnClone [1] = Instantiate (whatToSpawnPrefab[1], spawnLocations[1].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
 		whatToSpawnClone [2] = Instantiate (whatToSpawnPrefab[2], spawnLocations[2].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+
+	void Update () {
+		myCoolTimer -= Time.deltaTime;
+		if (myCoolTimer < 0 ) {
+			spawnSomethingAwesomePlease ();
+		}
 	}
 }
