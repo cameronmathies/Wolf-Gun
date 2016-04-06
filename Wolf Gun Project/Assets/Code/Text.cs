bef30c0e137e1c1fc13d15a2baa310f88d32ea38
cameronmathies <cmathies@mr.preble.com>
2016-04-06 12:12:16 -0400

    BAckup

--- Wolf Gun Project/Assets/Code/Text.cs
+++ Wolf Gun Project/Assets/Code/Text.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Text : MonoBehaviour {
 
 	 string myString = " Programmer \n Cameron Mathies \n \n Artists \n Amanda White \n Alec Koncinsky \n Noah Pottle  ";
+	public AudioSource Typing;
 	
 	// Use this for initialization
 	void Start () {
@@ -12,10 +13,18 @@ public class Text : MonoBehaviour {
 	
 	// Update is called once per frame
 	IEnumerator AutoType () {
+		Keys (); 
 		foreach(char letter in myString.ToCharArray()){
 			GetComponent<GUIText>().text += letter; //for unity 5x
 			// guiText.text += letter;  // for unity 4x
 			yield return new WaitForSeconds(0.3f);
 		}
+	}
+
+	void Keys(){
+		Typing = GetComponent<AudioSource> ();
+		Typing.Play ();
+		yield return new WaitForSeconds(0.3f);
+		Keys ();
 	}
 }
