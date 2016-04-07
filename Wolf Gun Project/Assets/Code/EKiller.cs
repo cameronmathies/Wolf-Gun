d6b65801ae1b61d90142c99ede6255998de27c2e
cameronmathies <cmathies@mr.preble.com>
2016-04-07 12:15:48 -0400

    Walfs can die

---
+++ Wolf Gun Project/Assets/Code/EKiller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EKiller : MonoBehaviour {
+
+	public GameObject Wolfs;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Bullet") {
+			Destroy(Wolfs);
+		}
+	}
+}
