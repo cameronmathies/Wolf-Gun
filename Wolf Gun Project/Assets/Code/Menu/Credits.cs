86ec877a31e825667dc2473d49569ed47720f4b7
cameronmathies <cmathies@mr.preble.com>
2016-04-11 13:15:23 -0400

    Names
    just need to get timing

--- Wolf Gun Project/Assets/Code/Menu/Credits.cs
+++ Wolf Gun Project/Assets/Code/Menu/Credits.cs
@@ -3,10 +3,23 @@ using System.Collections;
 
 public class Credits : MonoBehaviour {
 
+	public AudioSource NamesRead;
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Bullet"){
 			Application.LoadLevel("Credits");
 			//	Debug.Log("Quit");
 		}
+	}
+
+	void NameScene (){
+
+	}
+
+	IEnumerator Names(){
+		yield return new WaitForSeconds(34); 
+		Application.LoadLevel("Main Menu");
+
+		
+		
 	}
 }
