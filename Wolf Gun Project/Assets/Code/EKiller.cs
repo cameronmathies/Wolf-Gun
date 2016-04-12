7fb69047c369c033f1e0eeb97149f96d1feffec1
cameronmathies <cmathies@mr.preble.com>
2016-04-12 13:29:55 -0400

    Wolf Car
    The walf on the car can drive, shot and get killed.

--- Wolf Gun Project/Assets/Code/EKiller.cs
+++ Wolf Gun Project/Assets/Code/EKiller.cs
@@ -4,7 +4,18 @@ using System.Collections;
 public class EKiller : MonoBehaviour {
 
 	public GameObject Wolfs;
+	public float Kill;
 
+//	void OnEnable()
+//	{
+//	//	StartCoroutine(shot());
+//	}
+//	IEnumerator shot()
+//	{
+//	//	yield return new WaitForSeconds(Kill);
+//		//  Debug.Log("Test");
+//	//	gameObject.SetActive(false);
+//	} 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Bullet") {
 			Destroy(Wolfs);
